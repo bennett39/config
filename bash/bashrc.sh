0e2d62c5dd98d510727fbebb3b16e860c60093df
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2020-06-02 09:35:00 -0400

    Clean up bashrc

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,5 +1,4 @@
 # ~/.bashrc: executed by bash(1) for non-login shells.
-# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 
 # If not running interactively, don't do anything
 case $- in
@@ -7,38 +6,30 @@ case $- in
       *) return;;
 esac
 
-# don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
-HISTCONTROL=ignoreboth
-
-# append to the history file, don't overwrite it
+# History config
 shopt -s histappend
-
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
+HISTCONTROL=ignoreboth:erasedups
+HISTTIMEFORMAT="%Y-%m-%d %H:%M.%S | "
 
 # Locale
 export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+export LANG="C.UTF-8"
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
+# Check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-#shopt -s globstar
-
-# make less more friendly for non-text input files, see lesspipe(1)
+# Make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
-# set variable identifying the chroot you work in (used in the prompt below)
+# Set variable identifying the chroot you work in (used in the prompt below)
 if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# set a fancy prompt
+# Set a fancy prompt
 case "$TERM" in
     xterm-color|*-256color) color_prompt=yes;;
 esac
@@ -56,11 +47,7 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-
-# colored GCC warnings and errors
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# enable programmable completion features
+# Enable programmable completion features
 if ! shopt -oq posix; then
   if [ -f /usr/share/bash-completion/bash_completion ]; then
     . /usr/share/bash-completion/bash_completion
@@ -69,22 +56,6 @@ if ! shopt -oq posix; then
   fi
 fi
 #!/bin/bash
-
-# Git
-alias gs='git status'
-alias gp='git pull origin '
-alias gh='git push'
-alias ga='git add . && git status && ack "pdb"'
-alias gc='git commit -m '
-alias gl='git log --oneline'
-alias gb='git branch'
-alias gk='git checkout'
-
-# Vim
-alias v='vim '
-
-# Postgres start
-alias pgstart='sudo /etc/init.d/postgresql restart'
 
 # Directory traversal
 up(){
@@ -101,6 +72,7 @@ up(){
     cd $d
 }
 
+# Directory creation
 mkd(){
     mkdir -p $1
     cd $1
@@ -112,15 +84,27 @@ alias f='find . |grep '
 alias ls='ls -CHG'
 alias la='ls -ahG'
 alias ll='ls -ahlG'
+alias v='vim '
 
-# Aliases
+# Git
+alias gs='git status'
+alias gp='git pull origin '
+alias gh='git push'
+alias ga='git add . && git status && ack "pdb"'
+alias gc='git commit -m '
+alias gl='git log --oneline'
+alias gb='git branch'
+alias gk='git checkout'
+
+# Directory  & File Aliases
 alias vimrc='cd ~/Repos/config/vim/ && vim vimrc'
 alias bashrc='cd ~/Repos/config/bash/ && vim bashrc.sh'
-alias tmux='tmux -2'
-alias sp='tmux split-window -vc $PWD'
-alias vsp='tmux split-window -hc $PWD'
 alias repos='cd ~/Repos'
 alias cubeweb='cd ~/Repos/cube_planning/web'
+alias journal='cd ~/Repos/daily39 && vim journal.md'
+
+# Postgres start
+alias pgstart='sudo /etc/init.d/postgresql restart'
 
 # Cube aliases
 alias dockup='docker-compose up -d'
@@ -133,25 +117,19 @@ alias dockdj='docker exec -it cube_planning /bin/bash'
 alias dockdb='docker exec -it cube_planning_mysql_db /bin/bash'
 alias dockwork='docker logs -f cube_planning_worker'
 alias docklogs='docker logs -f cube_planning'
-alias postman='/usr/lib/Postman/Postman &; disown'
+alias ngrok='/Applications/ngrok'
 
 # Prompt color
 export PS1="\[\e[1m\e[95m\]\u\[\e[m\]\[\e[95m\]:\[\e[m\]\[\e[1m\e[96m\]\W\[\e[m\]\\$ "
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # Let there be color in grep!
 alias grep="grep --color=auto"
 
 # Set Vim as my default editor
 export EDITOR=vim
-
-# UTF-8 locale
-export LANG="C.UTF-8"
-
-# History config
-HISTCONTROL=ignoreboth:erasedups
-HISTSIZE=100
-HISTFILESIZE=2000
-HISTTIMEFORMAT="%Y-%m-%d %H:%M.%S | "
 
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
